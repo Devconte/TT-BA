7ce508df73208f67b1ad83e2fa0c8eb9b92ce4f7
Devconte <romain.lcnte@gmail.com>
2023-07-12 16:44:59 +0200

    using new template refacto

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App/App.tsx'
+import { Provider } from 'react-redux'
+import store from './store'
+import './index.css'
 
-import App from './components/App/App';
-
-import './styles/index.scss';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+ 
+)
